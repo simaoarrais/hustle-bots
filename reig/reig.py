58d1c3449994b4f3bccc3faf7316b19cbf567e32
simaoarrais <simaoarrais@gmail.com>
2023-07-23 05:58:22 +0100

    add reddit access

--- reig/reig.py
+++ reig/reig.py
@@ -8,15 +8,34 @@ def access_reddit():
     load_dotenv()
 
     # Access environment variables
-    username = os.getenv("USERNAME")
-    password = os.getenv("PASSWORD")
-    client_id = os.getenv("CLIENT_ID")
-    client_secret = os.getenv("CLIENT_SECRET")
+    REDDIT_USERNAME_ENV = os.getenv("REDDIT_USERNAME")
+    REDDIT_PASSWORD_ENV = os.getenv("REDDIT_PASSWORD")
+    CLIENT_ID_ENV = os.getenv("CLIENT_ID")
+    CLIENT_SECRET_ENV = os.getenv("CLIENT_SECRET")
 
-    print(f"username: {username}\
-          \npassword: {password}\
-          \nclient_id: {client_id}\
-          \nclient_secret: {client_secret}")
+    # print(f"username: {CLIENT_SECRET_ENV}\
+    #       \npassword: {REDDIT_PASSWORD_ENV}\
+    #       \nclient_id: {CLIENT_ID_ENV}\
+    #       \nclient_secret: {CLIENT_SECRET_ENV}")
+    
+    # Access Reddit API
+    reddit = praw.Reddit(
+        username=REDDIT_USERNAME_ENV,
+        password=REDDIT_USERNAME_ENV,
+        client_id=CLIENT_ID_ENV,
+        client_secret=CLIENT_SECRET_ENV,
+        user_agent="reig"
+    )
+    reddit.read_only = True
+
+    # Access a subreddit and posts
+    subreddit = reddit.subreddit("NatureIsFuckingLit")
+    posts = subreddit.search(query="desert", sort='new', time_filter='all', limit=1)
+
+    # Iterate through posts
+    for post in posts:
+        print(post.url)
+
     return 0
 
 if __name__ == "__main__":
