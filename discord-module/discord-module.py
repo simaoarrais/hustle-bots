b5f4bc4042c5e88d68bbdeaa246a7d182ebc1638
simaoarrais <simaoarrais@gmail.com>
2023-07-24 05:10:22 +0100

    add basic discord-module

---
+++ discord-module/discord-module.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+
+import discord
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access environment variables required for Discord authentication
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
