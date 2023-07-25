85286d7ccd995c408cf87b16a5bee8941bb172c7
simaoarrais <simaoarrais@gmail.com>
2023-07-25 04:37:47 +0100

    add post info on Discord

--- discord-module/discord-module.py
+++ discord-module/discord-module.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 
 import discord
+import json
 import os
 
 # Load environment variables from .env file
@@ -13,6 +14,11 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
+def read_json_file(filename):
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
@@ -24,5 +30,26 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+    elif message.content.startswith('$post_info'):
+        # Read the JSON data from the file
+        json_data = read_json_file('test.json')
+
+        # Process the post information and send a formatted message to the Discord channel
+        for post in json_data:
+            title = post['title']
+            score = post['score']
+            url = post['url']
+            author = post['author']
+            permalink = post['permalink']
+
+            # Format the message
+            post_info_message = f"**Title:** {title}\n" \
+                                f"**Score:** {score}\n" \
+                                f"**URL:** {url}\n" \
+                                f"**Author:** {author}\n" \
+                                f"**Permalink:** https://www.reddit.com{permalink}"
+
+            # Send the message to the Discord channel
+            await message.channel.send(post_info_message)
 
 client.run(TOKEN)
