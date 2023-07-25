625951fa5fbb2ccd5b7d3a3f6cc56d81c12a667f
simaoarrais <simaoarrais@gmail.com>
2023-07-25 04:26:47 +0100

    add function to save posts

--- reddit-module/reddit-module.py
+++ reddit-module/reddit-module.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import logging
 import praw
+import json
 import os
 
 def init_logger():
@@ -45,12 +46,13 @@ def access_subreddit(reddit, subreddit="NatureIsFuckingLit"):
 
 def get_top_posts(subreddit, threshold=1000, n_posts=5, search_limit=15):
     # Fetch the top posts from the subreddit based on the specified threshold and number of posts
-    posts = subreddit.top(time_filter='all', limit=search_limit)
+    top_posts = subreddit.top(time_filter='all', limit=search_limit)
 
     top_posts_above_threshold = []
 
-    for post in posts:
+    for post in top_posts:
         if len(top_posts_above_threshold) >= n_posts:
+            print("exit")
             break  # Stop iterating when the desired number of posts is reached
 
         if post.score > threshold and not post.is_video:
@@ -60,7 +62,31 @@ def get_top_posts(subreddit, threshold=1000, n_posts=5, search_limit=15):
 
     return top_posts_above_threshold
 
-if __name__ == "__main__":
+def save_posts(top_posts, filename="0"):
+    posts_data = []
+
+    for post in top_posts:
+        post_data = {
+            "title": post.title,
+            "score": post.score,
+            "url": post.url,
+            "author": post.author.name,
+            "created_utc": post.created_utc,
+            "is_video": post.is_video,
+            "permalink": post.permalink,
+            "num_comments": post.num_comments,
+            "thumbnail": post.thumbnail
+        }
+        posts_data.append(post_data)
+
+    # Convert the list of post dictionaries to a JSON-formatted string
+    json_data = json.dumps(posts_data, indent=4)
+    
+    # Write the JSON string to the specified file
+    with open(filename, 'w') as file:
+        file.write(json_data)
+
+def main():
     # Initialize the logger
     init_logger()
 
@@ -74,7 +100,13 @@ if __name__ == "__main__":
     subreddit = access_subreddit(reddit)
 
     # Get posts
-    get_top_posts(subreddit)
+    top_posts_above_threshold = get_top_posts(subreddit, threshold=1000, n_posts=1, search_limit=5)
+
+    # Save posts
+    save_posts(top_posts_above_threshold, filename="test.json")
 
     # Successful exiting
     exit(0)  # Log an INFO message to indicate successful exiting of the program
+
+if __name__ == "__main__":
+    main()
